ede91e4897ca229d8006a139500263f5f237775b
tubisaffo <tubisaffo@gmail.com>
2024-03-23 11:38:51 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(cats.length - 1);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+}
+
+function appendCat(name) {
+    return [...cats, 'Broom'];
+}
+
+function prependCat(name) {
+    return ['Arnold', ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
